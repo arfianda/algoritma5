4ec5bff86b2b434c3d1cea61979883c52a125de7
arfianda <arfiandafirsta@gmail.com>
2024-10-16 08:45:39 +0700

    initial commit

---
+++ pert5.py
@@ -0,0 +1,71 @@
+n = 4
+for i in range(0, n):
+    print(i)
+
+
+# count = 0
+# while (count > 4):
+#     count = count + 1
+#     print("Hello UPB")
+
+# count = 0
+# while (count < 3):
+#     count = count + 1
+#     print("Hello UPB")
+# else:
+#     print("In Else Block")
+
+
+# count = 0
+# while (count < 3):
+#     count = count + 1
+#     print("Hello Geek")
+
+# print("\nList Iteration")
+# l = ["geeks", "for", "geeks"]
+# for i in l:
+#     print(i)
+    
+# print("\nTuple Iteration")
+# t = ("UPB", "for", "UPBs")
+# for i in t:
+#     print(i)
+    
+# print("\nString Iteration")
+# s = "Megah"
+# for i in s:
+#     print(i)
+    
+# print("\nDictionary Iteration")
+# d = dict({'x':123, 'y':354})
+# for i in d:
+#     print("%s  %d" % (i, d[i]))
+    
+# print("\nSet Iteration")
+# set1 = {1, 2, 3, 4, 5, 6}
+# for i in set1:
+#     print(i)
+
+# from __future__ import print_function
+# for i in range(1, 5):
+#     for j in range(i):
+#         print(i, end=' ')
+#     print()
+# #======================
+ 
+# for letter in 'UPB':
+#     if letter == 'e' or letter == 's':
+#         continue
+#     print('Current Letter :', letter)
+# #=======================
+ 
+# for letter in 'UPB':
+#     if letter == 'e' or letter == 's':
+#         break
+# print('Current Letter :', letter)
+ 
+# #=========================
+ 
+# fruits = ["apple", "orange", "kiwi"]
+# for fruit in fruits:
+#     print(fruit)
